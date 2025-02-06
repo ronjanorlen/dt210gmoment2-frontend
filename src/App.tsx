978e71b27ecb0432106bed746bd58614225f878c
Ronja Norlén <rono2300@student.miun.se>
2025-02-06 13:48:38 +0100

    skapat hook för get-anrop

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
 import { useState, useEffect } from "react"; // Importera useState och useEffect
 import Todo from './components/Todo'; // Importera Todo-komponent
+import useGet from './hooks/useGet'; // Importera get-hook 
 
 //Interface for todo 
 interface todoInterface {
-  _id: string,
+  _id?: string,
   title: string,
   description: string,
   status: string
@@ -13,37 +14,8 @@ interface todoInterface {
 function App() {
 
   // States för komponent 
-  const [todos, setTodos] = useState<todoInterface[] | []>([])
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null)
-
-  useEffect(() => {
-    fetchData();
-  }, [])
-
-  // Hämta in data från api
-  const fetchData = async () => {
-    try {
-      setLoading(true);
-      const res = await fetch("https://dt210gmoment2-backend.onrender.com/todos");
-
-    // Om inte ok 
-      if (!res.ok) {
-        throw Error("Något gick fel vid hämtning av todos " + res.status);
-      }
-
-      // Bearbeta svar 
-      const data = await res.json();
-      setTodos(data);
-
-      // Fånga upp fel
-    } catch (error) {
-      setError("Det blev ett fel vid hämtning av todos");
-    } finally {
-      setLoading(false); // Vid klar med anrop 
-    }
-  }
-
+  const {data : todos, error, loading, fetchData} = useGet<todoInterface[]>("https://dt210gmoment2-backend.onrender.com/todos");
+  
   return (
     <>
     <main>
---
+++ src/hooks/useGet.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react"; // Importera useState och useEffect 
+
+// Hook för get-anrop 
+export default function useGet<T>(url: string) : {
+    data: T, // Representera todointerface 
+    error: string | null,
+    loading: boolean,
+    fetchData: () => void 
+} {
+     // States för komponent 
+      const [data, setData] = useState<T>([] as T) // Kan vara array av interface, eller bara objekt av interface 
+      const [loading, setLoading] = useState<boolean>(false);
+      const [error, setError] = useState<string | null>(null)
+
+      useEffect(() => {
+        fetchData();
+      }, [url]) // Om url ändras, kör anrop på nytt 
+    
+      // Hämta alla todos från api
+      const fetchData = async () => {
+        try {
+          setLoading(true);
+          const res = await fetch(url);
+    
+        // Om inte ok 
+          if (!res.ok) {
+            throw Error("Något gick fel vid hämtning av data " + res.status);
+          }
+    
+          // Bearbeta svar 
+          const data = await res.json();
+          setData(data);
+    
+          // Fånga upp fel
+        } catch (error) {
+          setError("Det blev ett fel vid hämtning av data");
+        } finally {
+          setLoading(false); // Vid klar med anrop 
+        }
+      }
+
+    return {data, error, loading, fetchData}
+}
